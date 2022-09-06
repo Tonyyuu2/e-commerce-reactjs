e3a3e6918d4d2ab97db8875eb3caee212c6cf5c4
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-06 16:56:24 -0700

    add home route

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,46 +1,13 @@
 import "./App.css";
-import CategoryItem from "./components/category-item/CategoryItem";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "../src/routes/home/Home.jsx";
 
 function App() {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
-  const categoryItem = categories.map((category) => {
-    return (
-      <CategoryItem key={category.id} category={category} />
-    )
-  } )
-
   return (
-    <div className="categories-container">
-      {categoryItem}
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
