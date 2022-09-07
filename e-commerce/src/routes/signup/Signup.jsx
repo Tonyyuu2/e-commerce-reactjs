fef2ac50700c2b943c2126ffa078050d68cc17c6
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-07 16:23:21 -0700

    refactor form field change functions and state

--- e-commerce/src/routes/signup/Signup.jsx
+++ e-commerce/src/routes/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // const defaultFormFields = {
 //   displayName: '',
@@ -8,40 +8,40 @@ import React, { useState } from 'react'
 // }
 
 function Signup() {
-
   const [state, setState] = useState({
-    displayName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  })
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  console.log('state :', state);
 
   const handleChange = (e) => {
-    const { name } = e;
-  }
-
+    e.preventDefault();
+    const { name, value } = e.target;
+    setState({...state, [name]: value})
+  };
 
   return (
     <div>
       <h1>Use what you got...</h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" name='displayName' required/>
+        <input type="text" name="displayName" onChange={handleChange} value={state.displayName} required />
 
         <label>Email</label>
-        <input type="email" name='email' required/>
+        <input type="email" name="email" value={state.email} onChange={handleChange} required />
 
         <label>Password</label>
-        <input type="password" name='password' required/>
+        <input type="password" name="password" value={state.password} onChange={handleChange} required />
 
         <label>Confirm Password</label>
-        <input type="password" name='confirmPassword'required/>
+        <input type="password" name="confirmPassword"   value={state.confirmPassword} onChange={handleChange} required />
 
-        <button type='submit'>Sign Up</button>
+        <button type="submit">Sign Up</button>
       </form>
-
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
