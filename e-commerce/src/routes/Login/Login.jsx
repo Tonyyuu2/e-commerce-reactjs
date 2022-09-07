c045e08e1a8126b5db6d1269ab18428f3fc61c8d
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-07 14:01:00 -0700

    add google login feature

--- e-commerce/src/routes/Login/Login.jsx
+++ e-commerce/src/routes/Login/Login.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { signInWithGooglePopup } from "../../utils/firebase.utils";
 
 function LogIn() {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <button onClick={logGoogleUser}>Login with Google</button>
     </div>
-  )
+  );
 }
 
-export default LogIn
+export default LogIn;
--- e-commerce/src/utils/firebase.utils.js
+++ e-commerce/src/utils/firebase.utils.js
@@ -1,4 +1,10 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsCKaMHt86PLHtSpJKTiIWlDvmoiuGCvw",
@@ -6,8 +12,16 @@ const firebaseConfig = {
   projectId: "new-ecommerce-67de6",
   storageBucket: "new-ecommerce-67de6.appspot.com",
   messagingSenderId: "522457829959",
-  appId: "1:522457829959:web:65e3842d156f1dac7cd762"
+  appId: "1:522457829959:web:65e3842d156f1dac7cd762",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup =  () => signInWithPopup(auth, provider)
