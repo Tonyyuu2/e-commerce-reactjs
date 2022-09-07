e4e202b14d70ccb1130adc537c5c6a957cf1dedf
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-07 13:36:52 -0700

    start firebase init

---
+++ e-commerce/src/utils/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsCKaMHt86PLHtSpJKTiIWlDvmoiuGCvw",
+  authDomain: "new-ecommerce-67de6.firebaseapp.com",
+  projectId: "new-ecommerce-67de6",
+  storageBucket: "new-ecommerce-67de6.appspot.com",
+  messagingSenderId: "522457829959",
+  appId: "1:522457829959:web:65e3842d156f1dac7cd762"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
