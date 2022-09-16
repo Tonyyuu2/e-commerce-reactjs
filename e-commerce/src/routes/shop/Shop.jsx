a6d212c9d303f4a1c48585eb1fbc922685d4bbc4
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-16 10:45:27 -0700

    add shopitems component

--- e-commerce/src/routes/shop/Shop.jsx
+++ e-commerce/src/routes/shop/Shop.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import ShopItems from './ShopItems'
+
+import SHOP_DATA from '../../shop-data/shop-data.json'
 
 function Shop() {
+
+  const shopItems = SHOP_DATA.map((item) => {
+    return (
+      <ShopItems key={item.id} item={item}/>
+    )
+  })
+
+
   return (
-    <div>Shop</div>
+    <div>
+      {shopItems}
+    </div>
   )
 }
 
---
+++ e-commerce/src/routes/shop/ShopItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShopItems({ item }) {
+  return (
+    <div>{item}</div>
+  )
+}
+
+export default ShopItems
