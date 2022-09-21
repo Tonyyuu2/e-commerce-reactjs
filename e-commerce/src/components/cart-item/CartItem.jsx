e0134f9544a8d78acf04157f12f86ae9e3c05d5d
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-09-21 14:06:28 -0700

    retry context creation'

---
+++ e-commerce/src/components/cart-item/CartItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './cartItem.styles.scss'
+
+function CartItem({cartItem}) {
+
+  const { name, quantity } = cartItem
+
+  return (
+    <div>
+      <h2>{name}</h2>
+    </div>
+  )
+}
+
+export default CartItem
