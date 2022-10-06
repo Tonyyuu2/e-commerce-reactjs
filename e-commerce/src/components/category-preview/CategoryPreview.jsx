6496b4d973da27160cfff1df125a1d47b83eaa53
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-10-06 16:51:44 -0700

    add styling and start to tackle shop bug

--- e-commerce/src/components/category-preview/CategoryPreview.jsx
+++ e-commerce/src/components/category-preview/CategoryPreview.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './categoryPreview.styles.scss'
-import ShopItems from '../../routes/shop/ShopItems'
+import React from "react";
+import "./categoryPreview.styles.scss";
+import ShopItems from "../../routes/shop/ShopItems";
 
 function CategoryPreview({ title, products }) {
   return (
-    <div className='category-preview-container'>
+    <div className="category-preview-container">
       <h2>
-        <span className='title'>{title.toUpperCase()}</span>
+        <span className="title">{title?.toUpperCase()}</span>
       </h2>
-      <div className='preview'>
-        {
-          products.filter((_, i) => i < 4).map((product) => (
-            <ShopItems key={product.id} product={product}/>
-          ))
-        }
+      <div className="preview">
+        {products
+          .filter((_, i) => i < 4)
+          .map((product) => {
+            return <ShopItems key={product.id} product={product} />;
+          })}
       </div>
     </div>
-  )
+  );
 }
 
-export default CategoryPreview
+export default CategoryPreview;
--- e-commerce/src/routes/shop/Shop.jsx
+++ e-commerce/src/routes/shop/Shop.jsx
@@ -7,16 +7,16 @@ import "./shop.styles.scss";
 function Shop() {
   const { categoriesMap } = useContext(CategoriesContext);
 
-  console.log("Object.keys(categoriesMap) :", Object.keys(categoriesMap));
+  const cateogryPreivew = Object.keys(categoriesMap).map((title) => {
+    const products = categoriesMap[title];
+    return (
+      <CategoryPreview key={title} title={title} products={products} />
+    );
+  })
 
   return (
     <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-        <CategoryPreview key={title} title={title} products={products}/>
-        )
-      })}
+      {cateogryPreivew}
     </div>
   );
 }
