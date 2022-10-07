b53bfe5ea4c1991e4718eeb8860e12d57a0dfb6e
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-10-07 15:15:25 -0700

    add optional chaining to wait for data

--- e-commerce/src/routes/category/Category.jsx
+++ e-commerce/src/routes/category/Category.jsx
@@ -1,9 +1,27 @@
-import './category.styles.scss'
+import "./category.styles.scss";
+import { useParams } from "react-router-dom";
+import { useContext, useState } from "react";
+import { CategoriesContext } from "../../context/categories.context";
+import { useEffect } from "react";
+import ShopItems from "../shop/ShopItems";
 
 function Category() {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
   return (
-    <div>Category</div>
-  )
+    <div className="category-container">
+      {products &&
+        products?.map((product) => (
+          <ShopItems key={product.id} item={product} />
+        ))}
+    </div>
+  );
 }
 
-export default Category
+export default Category;
