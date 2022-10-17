2cd857ce5f58d1ccc6fc690fc323e0766f946259
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-10-17 13:29:18 -0700

    refactor cartitem styling

--- e-commerce/src/components/cart-item/CartItem.jsx
+++ e-commerce/src/components/cart-item/CartItem.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import "./cartItem.styles.scss";
+import { CartItemContainer, ItemDetails } from "./cartItem.styles.jsx";
 
 function CartItem({ cartItem }) {
   const { name, quantity, price, imageUrl } = cartItem;
 
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={name} />
-      <div className="item-details">
-        <span className="name">{name}</span>
-        <span className="price">
-          {quantity} x ${price}
-        </span>
-      </div>
-    </div>
+    <CartItemContainer>
+      <img style={{ width: "30%" }} src={imageUrl} alt={name} />
+      <ItemDetails>
+        <span style={{ fontsize: "16px" }}>{name}</span>
+        <span style={{ fontsize: "16px" }}></span>
+        {quantity} x ${price}
+      </ItemDetails>
+    </CartItemContainer>
   );
 }
 
