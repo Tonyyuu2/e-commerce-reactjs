e7b31e8aea97ccc6641cfab786681f1e1aaf8e29
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-10-17 11:19:03 -0700

    convert button styles

--- e-commerce/src/components/button/Button.jsx
+++ e-commerce/src/components/button/Button.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import './button.styles.jsx'
+import { BaseButton, GoogleSignInButton, InvertedButton } from "./button.styles.jsx";
 
 const BUTTON_TYPE_CLASSES = {
-  google: 'google-sign-in',
-  inverted: 'inverted',
-}
+  google: "google-sign-in",
+  inverted: "inverted",
+};
 
 function Button({ children, buttonType, ...otherProps }) {
   return (
-    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps}>
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
       {children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
